3e8541503003cb5a4dcfdbff5dc458d7158485c2
Duong <softdevelop.inc@gmail.com>
2014-09-05 17:15:37 +0700

    fix admin module

--- trunk/themes/frontend/views/layouts/column2.php
+++ trunk/themes/frontend/views/layouts/column2.php
@@ -8,14 +8,6 @@
 	<div class="span-6 last">
 		<div id="sidebar">
 			<?php if(!Yii::app()->user->isGuest) $this->widget('UserMenu'); ?>
-
-			<?php $this->widget('TagCloud', array(
-				'maxTags'=>Yii::app()->params['tagCloudCount'],
-			)); ?>
-
-			<?php $this->widget('RecentComments', array(
-				'maxComments'=>Yii::app()->params['recentCommentCount'],
-			)); ?>
 		</div><!-- sidebar -->
 	</div>
 </div>
