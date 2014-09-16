7ee719ce1ec8acba95a97f56368689d447564dc2
duong <softdevelop.inc@gmail.com>
2014-09-16 16:30:50 +0700

    .gitignore is now working

--- trunk/themes/frontend/views/site/login.php
+++ trunk/themes/frontend/views/site/login.php
@@ -2,6 +2,6 @@
   <li><img src="<?php echo Yii::app()->request->baseUrl . WWWROOT_FRONTEND; ?>/images/instagram.png" alt="Instagram logo"></li>
   <li>
     <a class="login" href="<?php echo $loginUrl ?>">» Login with Instagram</a>
-    <h>Use your Instagram account to login.</h4>
+    <h4>Use your Instagram account to login.</h4>
   </li>
 </ul>
